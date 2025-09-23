21fab586d3cfb99803473315b9d742dc878a80ac
vac-end <sc220404@gmail.com>
2025-09-22 19:45:17 -0500

    feat: add login component with template, styles, and tests

---
+++ src/app/pages/website/login/login.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { AuthService } from '../../../../service/auth';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  imports: [],
+  templateUrl: './login.html',
+  styleUrl: './login.css'
+})
+export class Login {
+  email: string = '' ;
+  password: string = '';
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ){}
+
+  onLogin(){
+    this.authService.login(this.email, this.password).subscribe(() => {
+      this.router.navigate(['/plataforma']);
+    }, error =>{
+      console.error('Login failed', error)
+    });
+  }
+}
