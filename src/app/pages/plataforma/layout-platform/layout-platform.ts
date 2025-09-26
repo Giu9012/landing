af9eaac019432aaf3b086bab01c7ac478dc3528b
vac-end <sc220404@gmail.com>
2025-09-26 04:50:24 -0500

    antes de update angular

--- src/app/pages/plataforma/layout-platform/layout-platform.ts
+++ src/app/pages/plataforma/layout-platform/layout-platform.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Sidebar } from '../../../components/sidebar/sidebar';
 import { RouterOutlet } from '@angular/router';
+import { Side } from '../../../components/side/side';
 
 @Component({
   selector: 'app-layout-platform',
-  imports: [Sidebar, RouterOutlet],
+  imports: [ RouterOutlet, Side],
   templateUrl: './layout-platform.html',
   styleUrl: './layout-platform.css'
 })
